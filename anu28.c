b794b2e9ef3211d6f8dc24b32e3fdbcd3f301aec
anuu11 <35534904+anuu11@users.noreply.github.com>
2018-01-19 19:17:25 +0530

    Add files via upload

---
+++ anu28.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+void main()
+{
+  int a[100], index, size, c;
+ 
+  printf("Enter the number of elements in array\n");
+  scanf("%d", &size);
+ 
+  printf("Enter %d integers\n", size);
+ 
+  for (c = 0; c < size; c++)
+    scanf("%d", &a[c]);
+ 
+ 
+  printf("array elements are:");
+ for (c = 0; c < size; c++)
+ printf("index %d value %d",c,a[c]);
+}
